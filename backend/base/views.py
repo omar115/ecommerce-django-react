332fe37a2dfb89c6ce8305d555223ba6420d555d
omar115 <omarhasan115@gmail.com>
2021-03-24 01:04:00 +0600

    backend portion added with django and connect with react using axios

---
+++ backend/base/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'api/products/',
+        'api/products/create'
+    ]
+    # return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    # return JsonResponse(products, safe=False)
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    # return JsonResponse(products, safe=False)
+    # product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
