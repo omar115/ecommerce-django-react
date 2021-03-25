b8d6995cbd98632e61b8c932b4e583c5bdcb4b46
omar115 <omarhasan115@gmail.com>
2021-03-25 10:55:40 +0600

    database connect and migrations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
-          <HomeScreen />
+
         </Container>
       </main>
       <Footer />
