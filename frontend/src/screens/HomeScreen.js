c6e9e35e250145ce075a67fceeff49b2b317ad11
omar115 <omarhasan115@gmail.com>
2021-04-05 13:05:14 +0600

    msg2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,41 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import Product from "../components/Product"
-// import products from "../products"
-import axios from 'axios'
+import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+// import Paginate from '../components/Paginate'
+// import ProductCarousel from '../components/ProductCarousel'
+import { listProducts } from '../actions/ProductActions'
 
-function HomeScreen() {
-    const [products, setProducts] = useState([])
+
+function HomeScreen({ history }) {
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { error, loading, products } = productList
+
+    let keyword = history.location.search
 
     useEffect(() => {
-        // console.log('Use Effect trigger')
-        async function fetchProducts(){
-            const {data} = await axios.get('/api/products/')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts(keyword))
+
+    }, [dispatch, keyword])
 
     return (
         <div>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-            
+            <h1>Latast Products</h1>
+            {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    :
+                    <Row>
+                        {products.map(product => (
+                            <Col key= {product.id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+
+            }
         </div>
     )
 }
